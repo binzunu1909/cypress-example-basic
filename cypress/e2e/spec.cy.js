6328f3e7da11a454a919d24d381748a75c3b06e5
admin <hang.nguyen@restaff.no>
2023-06-28 10:44:49 +0700

    [Add]: Testcase research anything on Google Search Page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
-describe('Research on Google', () => {
-  it('passes', () => {
+describe('Google Search', () => {
+  it('should search for a query', () => {
+
     cy.visit('https://google.com')
+
+    cy.get('[name="q"]').type('OpenAI{enter}')
+
+    cy.wait(2000)
+
+    cy.title().should('contain', 'OpenAI')
+
   })
 })
